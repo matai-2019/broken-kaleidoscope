042041f182a08e57b0b427b8e8386bf9b535fcb5
Keith Yoon <uniyoona@naver.com>
2019-06-18 11:51:51 +1200

    done

---
+++ client/components/Pixel.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+class Pixel extends React.Component {
+  constructor () {
+    super()
+    this.state = {
+      style: {
+        height: 5,
+        width: 5,
+        backgroundColor: this.randomHexColor()
+      }
+    }
+  }
+
+  randomHexColor = () => `#${Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, 0)}`
+
+  render () {
+    return <div style = {this.state.style} > </div>
+  }
+}
+
+export default Pixel
