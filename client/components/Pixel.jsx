886a2e589abe9bd2b6c514185c81d91ff265ae5c
David Brown <david.brown@windowslive.com>
2019-06-18 11:03:04 +1200

    state of flux

--- client/components/Pixel.jsx
+++ client/components/Pixel.jsx
@@ -11,12 +11,23 @@ class Pixel extends React.Component {
       }
     }
   }
+
+  clickHandler = evt => {
+    this.setState({
+      style: {
+        height: '30px',
+        width: '30px',
+        backgroundColor: this.randomHexColor()
+      }
+    })
+  }
+
   randomHexColor = () =>
     `#${Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, 0)}`
 
   render () {
     return (
-      <div style={this.state.style}>
+      <div onClick={this.clickHandler} style={this.state.style}>
 
       </div>
     )
