d2001f02024e2908cc46709f8ba1102be435eac0
Celia Bailey <cabailey@xtra.co.nz>
2019-06-18 11:15:11 +1200

    set ccolor for exercise

--- client/components/Pixel.jsx
+++ client/components/Pixel.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import { generateKeyPair } from 'crypto';
 
 class Pixel extends React.Component {
   render () {
     return (
-      <div id = 'pixel'>Pretty</div>
+      <div id = 'pixel' style={{
+        backgroundColor: 'cornflowerblue',
+        height: '100px',
+        width: '300px'
+      }}></div>
     )
   }
 }
