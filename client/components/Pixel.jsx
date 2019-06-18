5b6e6139ba7949fe7b56ccf9c648f1b7977dd1d5
Joanna Klimas <klimas.j.m@gmail.com>
2019-06-19 07:55:01 +1200

    add random colors to pixels

--- client/components/Pixel.jsx
+++ client/components/Pixel.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
 
 class Pixel extends React.Component {
+  constructor () {
+    super()
+    this.state = {
+      height: 50,
+      width: 50,
+      backgroundColor: this.randomHexColor()
+    }
+    const randomHexColor = () =>
+    `#${Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, 0)}`
+  }
+
   render () {
     return (
-      <div style={{
-        height: 10,
-        width: 10,
-        backgroundColor: 'red'
-      }}>
-
+      <div style={this.state}>
       </div>
     )
   }
