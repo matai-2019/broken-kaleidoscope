cc29b16b643e329272fbca3378198d1451cb4c2c
Jared Tasker <jared@needapart.co.nz>
2019-06-18 15:52:04 +1200

    Change pixel color to black onContextMenu

--- client/components/Pixel.jsx
+++ client/components/Pixel.jsx
@@ -33,10 +33,20 @@ class Pixel extends React.Component {
       }
     })
   }
+
+  handleContextMenu = () => {
+    this.setState({
+      style: { 
+        backgroundColor: '#000000',
+        height: '120px',
+        width: '120px' 
+      }
+    })
+  }
   
   render () {
     return (
-      <div onClick = {this.handleClick} onMouseEnter = {this.handleMouseEnter} id = 'pixel' style = {this.state.style}></div>
+      <div onContextMenu={this.handleContextMenu} onClick = {this.handleClick} onMouseEnter = {this.handleMouseEnter} id = 'pixel' style = {this.state.style}></div>
     )
   }
 }
