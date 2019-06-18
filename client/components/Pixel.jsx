1ffc16fe45ca5add218c6942d9769002277b86dc
Joanna Klimas <klimas.j.m@gmail.com>
2019-06-19 07:47:40 +1200

    create Pixel component and set style properties

---
+++ client/components/Pixel.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+class Pixel extends React.Component {
+  render () {
+    return (
+      <div style={{
+        height: 10,
+        width: 10,
+        backgroundColor: 'red'
+      }}>
+
+      </div>
+    )
+  }
+}
+
+export default Pixel
