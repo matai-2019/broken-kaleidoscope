60feb8bdf3fcb53749782514b5997503b57209ac
Noel Soong <noelsoong@live.com>
2019-06-18 15:23:42 +1200

    Change color onmount

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Pixel from './Pixel'
 
 const App = () => {
-  return Array.from({ length: 100000 }, () => <Pixel />)
+  return Array.from({ length: 500 }, () => <Pixel size="25px" />)
 }
 
 export default App
--- client/components/Pixel.jsx
+++ client/components/Pixel.jsx
@@ -3,46 +3,57 @@ import React from 'react'
 class Pixel extends React.Component {
   constructor (props) {
     super(props)
+    this.size = this.props.size
     this.state = {
-      style: { backgroundColor: this.randomHexColor(), width: '5px', height: '5px' }
+      style: { backgroundColor: this.randomHexColor(), width: this.size, height: this.size }
     }
+  }
+
+  componentDidMount () {
+    setInterval(() => {
+      this.setState({
+        style: { backgroundColor: this.randomHexColor(),
+          width: this.size,
+          height: this.size }
+      })
+    }, 5000)
   }
 
   clickHandler = evt => {
     this.setState({
-      style: { backgroundColor: this.randomHexColor(), width: '5px', height: '5px' }
+      style: { backgroundColor: this.randomHexColor(), width: this.size, height: this.size }
     })
   }
 
   hoverHandler = evt => {
     this.setState({
       style: { backgroundColor: 'green',
-        width: '5px',
-        height: '5px' }
+        width: this.size,
+        height: this.size }
     })
   }
 
   contextHandler = evt => {
     this.setState({
       style: { backgroundColor: 'black',
-        width: '5px',
-        height: '5px' }
+        width: this.size,
+        height: this.size }
     })
   }
 
   doubleClickHandler = evt => {
     this.setState({
       style: { backgroundColor: 'white',
-        width: '5px',
-        height: '5px' }
+        width: this.size,
+        height: this.size }
     })
   }
 
   dragHandler = evt => {
     this.setState({
       style: { backgroundColor: 'yellow',
-        width: '5px',
-        height: '5px' }
+        width: this.size,
+        height: this.size }
     })
   }
 
@@ -57,7 +68,6 @@ class Pixel extends React.Component {
         onContextMenu={this.contextHandler}
         onDoubleClick={this.doubleClickHandler}
         onDragEnter={this.dragHandler}
-
         style={this.state.style}>
       </div>)
   }
