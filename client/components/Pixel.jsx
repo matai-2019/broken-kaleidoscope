ddbc769e9e0722a26d681943f2dbf66881f02fd9
Ruslan Makeev <dec32m@gmail.com>
2019-06-18 14:05:08 +1200

    made it better

--- client/components/Pixel.jsx
+++ client/components/Pixel.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 class Pixel extends React.Component {
-  constructor (props) {
+  constructor(props) {
     super(props)
     this.state = {
       style: {
@@ -11,12 +11,68 @@ class Pixel extends React.Component {
       }
     }
   }
+
   randomHexColor = () =>
     `#${Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, 0)}`
+
+  handleClick = evt => {
+    const newColor = this.randomHexColor()
+    this.setState({
+      style: {
+        backgroundColor: newColor,
+        height: 1,
+        width: 1
+      }
+    })
+  }
+
+  handleMouseEnter = evt => {
+    this.setState({
+      style: {
+        backgroundColor: 'green',
+        height: 1,
+        width: 1
+      }
+    })
+  }
+
+  handleDragEnter = evt => {
+    this.setState({
+      style: {
+        backgroundColor: 'yellow',
+        height: 1,
+        width: 1
+      }
+    })
+  }
+
+  handleContextMenu = evt => {
+    evt.preventDefault()
+    this.setState({
+      style: {
+        backgroundColor: 'black',
+        height: 1,
+        width: 1
+      }
+    })
+  }
+
+  handleDoubleClick = evt => {
+    this.setState({
+      style: {
+        backgroundColor: 'white',
+        height: 1,
+        width: 1
+      }
+    })
+  }
+
   render () {
     return (
-      <div style = {this.state.style}>
-      </div>
+      <>
+        <div style={this.state.style} onClick={this.handleClick} onMouseEnter={this.handleMouseEnter} onDragEnter={this.handleDragEnter} onContextMenu={this.handleContextMenu} onDoubleClick={this.handleDoubleClick}>
+        </div>
+      </>
     )
   }
 }
