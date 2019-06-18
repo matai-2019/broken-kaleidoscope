fbc8c229c5c6c5e487036421a6bcb67baa777292
Celia Bailey <cabailey@xtra.co.nz>
2019-06-18 15:14:36 +1200

    built colour grid

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -3,17 +3,19 @@ import React from 'react'
 import Pixel from './Pixel'
 
 class App extends React.Component {
+  pixelGrid (number) {
+    let pixelArr = []
+    for (let i = 0; i < number; i++) {
+      pixelArr.push(<Pixel/>)
+    }
+    return pixelArr
+  }
+
   render () {
     return (
-      <div>
-        <Pixel/>
-        <Pixel />
-        <Pixel />
-        <Pixel />
-        <Pixel />
-        <Pixel />
-        <Pixel />
-      </div>
+      <>
+        {this.pixelGrid(100)}
+      </>
     )
   }
 }
