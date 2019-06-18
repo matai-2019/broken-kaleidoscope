63524f22070ffb16dbb11834aea1493594818238
David Nicholson <davidnicholsonjob@gmail.com>
2019-06-18 15:46:11 +1200

    finished mvp and stretch

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,97 +1,21 @@
 import React from 'react'
 import Pixel from './Pixel'
 
-const App = () => {
-  
-  return (
-    <React.Fragment>
-      <Pixel />
-      <Pixel />
-      <Pixel />
-      <Pixel />
-      <Pixel />
-      <Pixel />
-      <Pixel />
-      <Pixel />
-      <Pixel />
-      <Pixel />
-      <Pixel />
-      <Pixel />
-      <Pixel />
-      <Pixel />
-      <Pixel />
-      <Pixel />
-      <Pixel />
-      <Pixel />
-      <Pixel />
-      <Pixel />
-      <Pixel />
-      <Pixel />
-      <Pixel />
-      <Pixel />
-      <Pixel />
-      <Pixel />
-      <Pixel />
-      <Pixel />
-      <Pixel />
-      <Pixel />
-      <Pixel />
-      <Pixel />
-      <Pixel />
-      <Pixel /> 
-      <Pixel />
-      <Pixel />
-      <Pixel />
-      <Pixel />
-      <Pixel />
-      <Pixel />
-      <Pixel />
-      <Pixel />
-      <Pixel />
-      <Pixel />
-      <Pixel />
-      <Pixel />
-      <Pixel />
-      <Pixel />
-      <Pixel />
-      <Pixel />
-      <Pixel /> 
-      <Pixel />
-      <Pixel />
-      <Pixel />
-      <Pixel />
-      <Pixel />
-      <Pixel />
-      <Pixel />
-      <Pixel />
-      <Pixel />
-      <Pixel />
-      <Pixel />
-      <Pixel />
-      <Pixel />
-      <Pixel />
-      <Pixel />
-      <Pixel />
-      <Pixel /> 
-      <Pixel />
-      <Pixel />
-      <Pixel />
-      <Pixel />
-      <Pixel />
-      <Pixel />
-      <Pixel />
-      <Pixel />
-      <Pixel />
-      <Pixel />
-      <Pixel />
-      <Pixel />
-      <Pixel />
-      <Pixel />
-      <Pixel />
-      <Pixel />
-      <Pixel />
-    </ React.Fragment>
-  )
+  class App extends React.Component {
+    pixelArray (n) {
+      let arr = []
+      for (let i = 0; i < n; i++) {
+        arr.push(<Pixel />)
+      }
+      return arr
+    }
+  render () {
+    return (
+      <React.Fragment>
+        {this.pixelArray(100)}
+      </ React.Fragment>
+    )
+  }
 }
 
 export default App
--- client/components/Pixel.jsx
+++ client/components/Pixel.jsx
@@ -3,13 +3,10 @@ import React from 'react'
 class Pixel extends React.Component {
 
   randomHexColor = () =>
-  `#${Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, 0)}`
-
-  randomSize = () =>
-  `#${Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, 0)}`
-
+    `#${Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, 0)}`
 
   constructor() {
+
     super()
 
     this.state = {
@@ -22,20 +19,67 @@ class Pixel extends React.Component {
   }
 
   clickHandler = () => {
-    // const newColor = this.state.style.backgroundColor
+    if (this.state.style.backgroundColor === 'blue') {
+      this.setState({
+        style: {
+          height: 200,
+          width: 200,
+          backgroundColor: 'magenta'
+        }
+      })
+    } else {
+      this.setState({
+        style: {
+          height: 200,
+          width: 200,
+          backgroundColor: this.randomHexColor()
+        }
+      })
+    }
+  }
+
+  rightClick = () => {
     this.setState({
       style: {
         height: 200,
         width: 200,
-        backgroundColor: this.randomHexColor()
+        backgroundColor: 'black'
       }
     })
+  }
+
+  doubleClick = () => {
+    this.setState({
+      style: {
+        height: 200,
+        width: 200,
+        backgroundColor: 'white'
+      }
+    })
+  }
+
+  dragEnter = () => {
+    this.setState({
+      style: {
+        height: 200,
+        width: 200,
+        backgroundColor: 'yellow'
+      }
+    })
+  }
+
+  componentDidMount () {
+    const intervalId = setInterval(() => this.clickHandler(), 100); this.setState({ intervalId })
+  }
+
+  componentWillUnmount () {
+    clearInterval(this.state.intervalId)
   }
 
   render() {
     return (
       <React.Fragment>
-        <div style={ this.state.style } onMouseEnter={ this.clickHandler }>
+        <div style={this.state.style} onMouseEnter={this.clickHandler} onContextMenu={this.rightClick} onDoubleClick={this.doubleClick} onDragEnter={this.dragEnter}>
         </div>
       </React.Fragment>
     )
@@ -43,3 +87,4 @@ class Pixel extends React.Component {
 }
 
 export default Pixel
+
