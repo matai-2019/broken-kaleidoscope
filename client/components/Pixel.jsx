cd598bf840569e6eb917d2440fc77ffde8f99071
Jared <jared@needapart.co.nz>
2019-06-18 19:49:37 +1200

    handleClick generates generates a new colour for the pixel

--- client/components/Pixel.jsx
+++ client/components/Pixel.jsx
@@ -14,12 +14,21 @@ class Pixel extends React.Component {
 
   render() {
     return (
-      <div style = {this.state.style}></div>
+      <div style = {this.state.style} onClick = {this.handleClick}></div>
     )
   }
 
-  randomHexColor = () =>
-  `#${Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, 0)}`
+  randomHexColor = () => `#${Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, 0)}`
+
+  handleClick = evt => {
+    this.setState({
+      style: {
+        height: '10px',
+        width: '10px',
+        backgroundColor: this.randomHexColor()
+      }
+    })
+  }
 
 }
 
