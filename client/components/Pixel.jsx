fce1ef5e9cf3c14b4bd2710e33be96515070ff50
Joanna Klimas <klimas.j.m@gmail.com>
2019-06-19 17:51:56 +1200

    add click and mouse enter handlers

--- client/components/Pixel.jsx
+++ client/components/Pixel.jsx
@@ -1,21 +1,37 @@
 import React from 'react'
 
 
+
 class Pixel extends React.Component {
   constructor () {
     super()
+    const randomHexColor = () =>
+  `#${Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, 0)}`
     this.state = {
       height: 50,
       width: 50,
       backgroundColor: randomHexColor()
     }
-    const randomHexColor = () =>
-  `#${Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, 0)}`
   }
-  
+
+  handleClick = (evt) => {
+    this.setState({
+      backgroundColor: 'black'
+    })
+  }
+
+  handleMouseEnter = (evt) => {
+    this.setState({
+      backgroundColor: 'yellow'
+    })
+  }
+
+
+
+
   render () {
     return (
-      <div style={this.state}>
+      <div onClick={this.handleClick} onMouseEnter={this.handleMouseEnter} style={this.state}>
       </div>
     )
   }
